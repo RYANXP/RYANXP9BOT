8ab119fc550b54f8ea4229aaada416e46e44a2da
RYANXP <76768535+RYANXP@users.noreply.github.com>
2021-01-01 15:59:35 -0300

    Add files via upload

---
+++ scr/help.js
@@ -0,0 +1,99 @@
+const help = (prefix) => {
+	return `*[🐊RYANXPBOT9🐊]*
+
+*_🖼️COMANDOS DE FIGURINHAS🖼️_*
+
+!sticker ou !stiker
+*Figurinhas*
+
+!sticker nobg
+*figurinhas sem fundo*
+
+!toimg
+*figurinha para imagem*
+
+!tsticker ou !tstiker
+*Figurinha Texto*
+______________________________
+
+*_COMANDOS DE MEME_*
+
+!meme
+*Memes EUA*
+
+!memeindo
+*Memes INDONESIA*
+______________________________
+
+*_🛩️OUTROS COMANDOS🛩️_*
+
+!gtts
+*Converte texto en Audio*
+use: *!gtts [idioma] [texto]*
+exemplo: *!gtts pt Oiie:)*
+
+!loli
+*Fotos de lolis*
+
+!nsfwloli
+*Fotos Aleatorias de nsfwloli*
+
+!url2img
+*capturas de tela da web*
+exemplo: *!url2img [tipe] [url]*
+
+!simi
+*Sua mensagem sera respondia pela simi*
+exemplo: *!simi oi*
+
+!ocr
+*Tira texto da imagem*
+
+!wait
+*Pesquisar anime por imagem*
+exemplo: *!wait e Mande a foto do anime*
+
+!setprefix
+*Mudar prefixo*
+obs: *so pode ser usado pelo propietario do bot.*
+_________________________________
+
+*_COMANDOS DE GRUPO_*
+
+~PARA USAR ESSES COMANDOS O BOT PRECISA DE ADM.~
+
+!add
+*Adicione Pessoas ao grupo*
+exemplo: *!add 55119683xxxx*
+
+!kick
+*Bane uma pessoa*
+exemplo: *!kick @tagmembro*
+
+!promote
+*Promove A pessoa Pra adm*
+exemplo: *!promote @tagmembro*
+
+!demote
+*Tire o adm da pessoa*
+exemplo: *!demote @tagmembro*
+
+!linkgroup
+*Link do grupo*
+
+!listadmins
+*Menciona adms*
+
+!tagall
+*Menciona Todos os membros*
+
+!welcome
+*Ativa o Bem vindo no grupo e desativa*
+exemplo: *!welcome 1 Liga !welcome 0 desliga*
+
+!simih
+*Ativa o Simih no grupo*
+exemplo: *!simih 1 ativa !simih 0 desativa.*`
+}
+
+exports.help = help
